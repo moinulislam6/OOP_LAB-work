51d1e9faf2d4074c67ef9691c2f48ca00db42f75
Moinul Islam <moinulislam42062@gmail.com>
2023-03-10 00:09:28 +0600

    Main.java

--- Lab 08/Code-02/Main.java
+++ Lab 08/Code-02/Main.java
@@ -3,9 +3,9 @@ package code2;
 public class Main {
     public static void main(String[] args) {
         AddressInfo addressInfo = new AddressInfo("Sector-7","Uttara","Dhaka","1711");
-        InsuranceInfo insuranceInfo = new InsuranceInfo("Life Insurance",5658);
+        InsuranceInfo insuranceInfo = new InsuranceInfo("Life Insurance",4652);
 
-        Employee e1 = new Employee("Abdullah Al Mahmud",addressInfo,insuranceInfo);
+        Employee e1 = new Employee("Moinul Islam",addressInfo,insuranceInfo);
         e1.display();
 
         }
